117496dd54302a475dd9f7c46bf7cb884f03c93f
Davi Alexandre <davi@davialexandre.com.br>
2015-07-22 22:14:06 -0300

    Enable Auto-Upgrade of VMware Tools
    This fixes the problem with the HGFS kernel module that wouldn't
    load after a kernel update

--- scripts/fusion.sh
+++ scripts/fusion.sh
@@ -10,3 +10,5 @@ tar zxf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
 rm /home/vagrant/linux.iso
 umount /mnt/cdrom
 apt-get -y purge build-essential linux-headers-$(uname -r)
+sed -i.bak 's/answer AUTO_KMODS_ENABLED_ANSWER no/answer AUTO_KMODS_ENABLED_ANSWER yes/g' /etc/vmware-tools/locations
+sed -i 's/answer AUTO_KMODS_ENABLED no/answer AUTO_KMODS_ENABLED yes/g' /etc/vmware-tools/locations
